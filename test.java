783278edcedd59aacab67326205b8317fb980b29
thoanggg <163012258+thoanggg@users.noreply.github.com>
2024-03-22 08:36:54 +0700

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+
+
+public class test {
+
+    public static void main(String[] args) {
+        
+        
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.print("a");
+    }
+}
+
+
+
